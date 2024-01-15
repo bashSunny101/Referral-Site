08163ebfd1349efb61cd861cc0ff1cdf1248b399
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2024-01-16 02:36:15 +0530

    app.css deleted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import RefreeSignUp from './Components/RefreeSignUp'
 
 function App() {
