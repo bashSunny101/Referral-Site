337e5dae5ac3b1c846e448bde8071002c823241c
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2024-01-18 02:38:42 +0530

    signuprefree

--- frontend/src/Components/RefreeSignUp.jsx
+++ frontend/src/Components/RefreeSignUp.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import './RefreeSignUp'
+import './RefreeSignUp.css'
+// import logo from '../assets/logoimg'
 
 function RefreeSignUp() {
   return (
     <>
-    <div id='background'  className='h-screen w-screen'>
-hello
+    <div id='background' >
+      <div id='nav' >
+      <div id='logo' >
+        <img src={'../assets/logoimg.png' }/>
+        <span id='your' >YOUR </span>
+        <span id='logo' >LOGO</span>
+        </div>
+      </div>
+      <div id='connectingtalents' >
+      <div id='connecting'>Connecting  </div>
+      <div id='talents' >Talents  </div>
+      </div>
+      <div id='new'>
+      <div id='createnew'>Create a new account </div>
+      </div>
+      <div id='signin'>
+        <span id='already'> already have one! <a href='www.google.com'>sign in here</a></span>
+      </div>  
     </div>
       
     </>
